1ca726fb05c3c7445677a02894f41e9c8ec0f4f3
AmelieYaacoub <a.yaacoub@gmx.de>
2020-05-27 20:11:58 +0200

    Add Toolbar

--- app/src/main/java/com/example/wikipedialanguage/GameTextActivity.java
+++ app/src/main/java/com/example/wikipedialanguage/GameTextActivity.java
@@ -1,6 +1,7 @@
 package com.example.wikipedialanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +9,17 @@ import android.view.View;
 
 public class GameTextActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_text);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
     public void GoBack(View V){
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
@@ -19,4 +27,5 @@ public class GameTextActivity extends AppCompatActivity {
     }
 
 
+    
 }
--- app/src/main/java/com/example/wikipedialanguage/MainActivity.java
+++ app/src/main/java/com/example/wikipedialanguage/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.wikipedialanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,8 +13,6 @@ import com.example.wikipedialanguage.Services.Animal;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
