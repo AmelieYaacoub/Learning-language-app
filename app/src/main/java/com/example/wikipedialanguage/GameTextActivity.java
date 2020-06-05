12747a9b128374153e6a3fd63344cacb92f78dc4
AmelieYaacoub <a.yaacoub@gmx.de>
2020-06-05 14:55:51 +0200

    Connection to Wiki Api

--- app/src/main/java/com/example/wikipedialanguage/GameTextActivity.java
+++ app/src/main/java/com/example/wikipedialanguage/GameTextActivity.java
@@ -5,21 +5,42 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.wikipedialanguage.Services.APIConnectivityService;
+
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class GameTextActivity extends AppCompatActivity {
-
+    private Button button;
     Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_text);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        APIConnectivityService APIConnection = new APIConnectivityService();
+
+
+      //  toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        TextView mainText = (TextView)findViewById(R.id.textView3);
+
+        try {
+            String something = APIConnection.execute().get();
+            mainText.setText(something);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
     }
     public void GoBack(View V){
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
---
+++ app/src/main/java/com/example/wikipedialanguage/Services/APIConnectivityService.java
@@ -0,0 +1,35 @@
+package com.example.wikipedialanguage.Services;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class APIConnectivityService  extends AsyncTask<String,String,String> {
+    private String GetFlatWikipediaText(){
+        try{
+            URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=description&titles=Portugal&formatversion=2");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            return content.toString();
+        }catch(IOException E){
+            return "Connection error";
+        }
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return GetFlatWikipediaText();
+    }
+}
